f242db3bfcdbafa256b1c706a36acf392d22f3e2
WangShaoqiang <WangShaoqiang@huazhu.com>
2020-04-26 08:28:37 +0800

    添加theamleaf logback日志等

--- src/main/java/com/feiwangSpring/controller/thymeleafController.java
+++ src/main/java/com/feiwangSpring/controller/thymeleafController.java
@@ -1,6 +1,9 @@
 package com.feiwangSpring.controller;
 
 import com.feiwangSpring.model.User;
+import com.feiwangSpring.service.impl.GenerateCodeServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +18,7 @@ import java.util.Date;
  */
 @Controller
 public class thymeleafController {
+    private static final Logger logger = LoggerFactory.getLogger(thymeleafController.class);
     @RequestMapping(value = "/test",method = RequestMethod.GET)
     public String test(Model model) {
         model.addAttribute("msg","wsq test thymeleaf");
@@ -22,6 +26,9 @@ public class thymeleafController {
     }
     @RequestMapping(value = "/message/member_show", method = RequestMethod.GET)
     public String memberShow(Model model) {
+        logger.info("进入memberShow");
+        logger.debug("debug membershow");
+        logger.error("error membershow");
         User vo = new User();
         vo.setUid(12345678L);
         vo.setName("尼古拉丁.赵四");
